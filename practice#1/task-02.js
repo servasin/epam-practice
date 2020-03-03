46bf6ee36709777b4d2daa09677dbbaaf9ee07a7
Sergey Vasin <vvs221@bk.ru>
2020-03-03 22:25:12 +0300

    added task 2

--- practice#1/task-02.js
+++ practice#1/task-02.js
@@ -1,25 +1,36 @@
 'use strict'
 
 function countNumerals(num) {
-  if (typeof num !== 'number' || Number.isNaN(num)) {
+  if (typeof num === 'number' && !Number.isNaN(num)) {
     let signOfNumber;
-    let numeralsCounter;
+    let digitAmount;
     if(num < -99 || num > 99) {
-      return 'number must be -99 < num < 99';
+      return 'число должно быть больше -100 и меньше 100';
     }
     if(num === 0) {
-      signOfNumber = 'it\'s zero';
+      signOfNumber = ' это ноль';
+      return num + signOfNumber;
     }
     if(num  > 0) {
-      signOfNumber = 'positive';
+      signOfNumber = ' положительное';
     }
     if(num  < 0) {
-      signOfNumber = 'negative';
+      signOfNumber = ' отрицательное';
     }
-    for(let i = 0; n > 1; i++) {
-      n /= 10;
-   }
+    if( (Math.abs(num) / 10) < 10) {
+       digitAmount = 'состоит из двух цифр';
+    }
+    if( (Math.abs(num) / 10) < 1) {
+       digitAmount = 'состоит из одной цифры';
+    }
 
+    return `число ${num} ${digitAmount}, ${signOfNumber}`;
   }
   return undefined;
 }
+
+console.log(countNumerals(1));
+console.log(countNumerals(-10));
+console.log(countNumerals(99));
+console.log(countNumerals(NaN));
+console.log(countNumerals(-Infinity));
